c45884f40553e5a76bd941bb6495ab6485932365
aieuo <35859665+aieuo@users.noreply.github.com>
2018-06-10 23:34:54 +0900

    Add files via upload

---
+++ ReplenishResources_v0.1/src/aieuo/ReplenishResources.php
@@ -0,0 +1,194 @@
+<?php
+namespace aieuo;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\Player;
+use pocketmine\block\Block;
+use pocketmine\utils\Config;
+use pocketmine\math\Vector3;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\command\ConsoleCommandSender;
+
+class ReplenishResources extends PluginBase implements Listener{
+    public function onEnable(){
+        $this->getServer()->getPluginManager()->registerEvents($this,$this);
+        if(!file_exists($this->getDataFolder())) @mkdir($this->getDataFolder(), 0721, true);
+    	$this->config = new Config($this->getDataFolder()."config.yml", Config::YAML);
+    }
+
+  	public function onCommand(CommandSender $sender, Command $command,string $label, array $args):bool{
+        $cmd = $command->getName();
+    	if($cmd == "reso"){
+    		if(!$sender instanceof Player){
+    			$sender->sendMessage("コンソールからは使用できません");
+    			return true;
+    		}
+    		if(!isset($args[0])){
+    			return false;
+    		}
+        	$name = $sender->getName();
+    		switch ($args[0]) {
+				case 'pos1':
+					$this->pos1break[$name] = true;
+					unset($this->pos2[$name]);
+					$sender->sendMessage("ブロックを壊してください");
+					return true;
+				case 'pos2':
+					if(!isset($this->pos1[$name])){
+						$sender->sendMessage("まずpos1を設定してください");
+						return true;
+					}
+					$this->pos2break[$name] = true;
+					$sender->sendMessage("ブロックを壊してください");
+					return true;
+				case 'add':
+					if(!isset($args[1])){
+						$sender->sendMessage("/reso add <id>");
+						return true;
+					}
+					if(!isset($this->pos1[$name]) or !isset($this->pos2[$name])){
+						$sender->sendMessage("まずposを設定してください");
+						return true;
+					}
+					$this->addbreak[$name] = $args[1];
+					$sender->sendMessage("ブロックを壊してください");
+					return true;
+    		}
+    		return true;
+		}
+	}
+
+	public function onBreak(BlockBreakEvent $event){
+		$player = $event->getPlayer();
+		$name = $player->getName();
+		if(isset($this->pos1break[$name])){
+			$event->setCancelled();
+			$block = $event->getBlock();
+			$this->pos1[$name] = [
+				"x" => $block->x,
+				"y" => $block->y,
+				"z" => $block->z,
+				"level" => $block->level->getFolderName()
+			];
+			unset($this->pos1break[$name]);
+			$player->sendMessage("設定しました(".$this->pos1[$name]["x"].",".$this->pos1[$name]["y"].",".$this->pos1[$name]["z"].",".$this->pos1[$name]["level"].")");
+		}elseif(isset($this->pos2break[$name])){
+			$event->setCancelled();
+			$block = $event->getBlock();
+			if($this->pos1[$name]["level"] != $block->level->getFolderName()){
+				$player->sendMessage("pos1と同じワールドに設定してください");
+				return;
+			}
+			$this->pos2[$name] = [
+				"x" => $block->x,
+				"y" => $block->y,
+				"z" => $block->z,
+				"level" => $block->level->getFolderName()
+			];
+			unset($this->pos2break[$name]);
+			$player->sendMessage("設定しました");
+		}elseif(isset($this->addbreak[$name])){
+			$block = $event->getBlock();
+			$event->setCancelled();
+			if(!($block->getId() == 63 or $block->getId() == 68))return;
+			$ids = explode(":",$this->addbreak[$name]);
+			if(!isset($ids[1]))$ids[1] = 0;
+			$this->config->set($block->x.",".$block->y.",".$block->z.",".$block->level->getFolderName(),[
+				"startx" => min($this->pos1[$name]["x"],$this->pos2[$name]["x"]),
+				"starty" => min($this->pos1[$name]["y"],$this->pos2[$name]["y"]),
+				"startz" => min($this->pos1[$name]["z"],$this->pos2[$name]["z"]),
+				"endx" => max($this->pos1[$name]["x"],$this->pos2[$name]["x"]),
+				"endy" => max($this->pos1[$name]["y"],$this->pos2[$name]["y"]),
+				"endz" => max($this->pos1[$name]["z"],$this->pos2[$name]["z"]),
+				"level" => $this->pos1[$name]["level"],
+				"id" => [
+					"id" => $ids[0],
+					"damage" => $ids[1]
+				]
+			]);
+			$this->config->save();
+			$player->sendMessage("追加しました");
+			unset($this->addbreak[$name]);
+		}
+	}
+
+    public function onTouch(PlayerInteractEvent $event){
+    	$player = $event->getPlayer();
+    	$block = $event->getBlock();
+    	if(($block->getId() == 63 or $block->getId() == 68) and $player->isSneaking()){
+    		$place = $block->x.",".$block->y.",".$block->z.",".$block->level->getFolderName();
+    		$time = $this->checkTime($player->getName(),$place);
+    		if($time == false){
+    			$player->sendMessage("1分以内に使用しています\nしばらくお待ちください");
+    			return;
+    		}
+	    	if($this->config->exists($place)){
+	    		$datas = $this->config->get($place);
+	    		$count = $this->countBlocks($datas);
+	    		if($count != 0){
+	    			$player->sendMessage("まだブロックが残っています");
+	    			return;
+	    		}
+	    		$this->setBlocks($datas);
+	    	}
+	    }
+    }
+
+    public function checkTime($name,$type){
+    	if(!isset($this->time[$name][$type])){
+			$this->time[$name][$type] = microtime(true);
+			return true;
+    	}
+    	$time = microtime(true) -$this->time[$name][$type];
+    	if($time <= 60){
+    		return false;
+    	}
+		$this->time[$name][$type] = microtime(true);
+    	return true;
+    }
+
+    public function countBlocks($datas){
+    	$sx = $datas["startx"];
+    	$sy = $datas["starty"];
+    	$sz = $datas["startz"];
+    	$ex = $datas["endx"];
+    	$ey = $datas["endy"];
+    	$ez = $datas["endz"];
+    	$level = $this->getServer()->getLevelByName($datas["level"]);
+    	$count = 0;
+    	for ($x = $sx; $x <= $ex; $x++) {
+	    	for ($y = $sy; $y <= $ey; $y++) {
+		    	for ($z = $sz; $z <= $ez; $z++) {
+		    		$block = $level->getBlock(new Vector3($x,$y,$z));
+		    		if($block->getId() !== 0){
+		    			$count ++;
+		    		}
+		    	}
+	    	}
+    	}
+    	return $count;
+    }
+
+    public function setBlocks($datas){
+    	$sx = $datas["startx"];
+    	$sy = $datas["starty"];
+    	$sz = $datas["startz"];
+    	$ex = $datas["endx"];
+    	$ey = $datas["endy"];
+    	$ez = $datas["endz"];
+    	$level = $this->getServer()->getLevelByName($datas["level"]);
+    	$id = $datas["id"]["id"];
+    	$meta = $datas["id"]["damage"];
+    	for ($x = $sx; $x <= $ex; $x++) {
+	    	for ($y = $sy; $y <= $ey; $y++) {
+		    	for ($z = $sz; $z <= $ez; $z++) {
+		    		$level->setBlock(new Vector3($x,$y,$z),Block::get($id,$meta));
+		    	}
+	    	}
+    	}
+    }
+}
